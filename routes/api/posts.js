3733b74401c4a58433bf04fa630a4d7657ceeed0
mkt-outform <56084025+richardNovoa@users.noreply.github.com>
2021-11-23 22:58:38 -0500

    delete a comment

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -152,4 +152,75 @@ router.put('/unlike/:id', auth, async (req, res) => {
   }
 });
 
+//@route    POST api/posts/comment/:id
+//@desc     Add a comment
+//@access   Private
+
+router.post(
+  '/comment/:id',
+  [auth, [check('text', 'Text is required').not().isEmpty()]],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const user = await User.findById(req.user.id).select('-password');
+      const post = await Post.findById(req.params.id);
+
+      const newComment = {
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id,
+      };
+      post.comments.unshift(newComment);
+      await post.save();
+      res.json(post.comments);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
+
+//@route    DELETE api/posts/comment/:id/:comment_id
+//@desc     Delete a comment
+//@access   Private
+
+router.delete('/comment/:id/:comment_id', auth, async (req, res) => {
+  try {
+    //calling the relevant post
+    const post = await Post.findById(req.params.id);
+    //defining the comment to delete in the post comments array
+    const comment = post.comments.find(
+      (comment) => comment.id === req.params.comment_id
+    );
+    //checking if comment exists
+    if (!comment) {
+      return res.status(404).json({ msg: 'Comment not found' });
+    }
+    //checking if current user is authorized
+    if (comment.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'User unauthorized' });
+    }
+    //indexing comment from the comments array
+    //prettier-ignore
+    const removeIndex = post.comments
+      .map(comment => comment.user.toString())
+      .indexOf(req.user.id);
+
+    //splicing comment from comments
+    post.comments.splice(removeIndex, 1);
+    //saving to database
+    post.save();
+    //responding with success message
+    res.json({ msg: 'Comment succesfully deleted' });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
 module.exports = router;
