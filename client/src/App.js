42d0c0b02c6a2c53b90c9eba11f3d6479bc5b7cc
Richard Novoa <56084025+richardNovoa@users.noreply.github.com>
2022-04-27 17:02:10 -0400

    added alert in redux

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,8 @@ import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+import Alert from './components/layout/Alert';
+
 import './App.css';
 //Redux
 import { Provider } from 'react-redux';
@@ -15,11 +17,14 @@ export default function App() {
 			<Router>
 				<Fragment>
 					<Navbar />
-					<Routes>
-						<Route path='/' element={<Landing />} />
-						<Route path='/register' element={<Register />} />
-						<Route path='/login' element={<Login />} />
-					</Routes>
+					<section className='container'>
+						<Alert />
+						<Routes>
+							<Route path='/' element={<Landing />} />
+							<Route path='/register' element={<Register />} />
+							<Route path='/login' element={<Login />} />
+						</Routes>
+					</section>
 				</Fragment>
 			</Router>
 		</Provider>
