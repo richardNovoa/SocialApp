da07b57fee20511bafbafb8be59b94a30aa3bd18
Richard Novoa <56084025+richardNovoa@users.noreply.github.com>
2022-05-10 22:55:55 -0400

    added user_loaded & auth_failed

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,24 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
-
-import './App.css';
-//Redux
+import { loadUser } from './actions/auth';
+import setAuthToken from './utils/setAuthToken';
 import { Provider } from 'react-redux';
 import store from './store';
+import './App.css';
 
-export default function App() {
+if (localStorage.token) {
+	setAuthToken(localStorage.token);
+}
+//Redux
+const App = () => {
+	useEffect(() => {
+		store.dispatch(loadUser());
+	}, []);
+
 	return (
 		<Provider store={store}>
 			<Router>
@@ -25,4 +33,5 @@ export default function App() {
 			</Router>
 		</Provider>
 	);
-}
+};
+export default App;
