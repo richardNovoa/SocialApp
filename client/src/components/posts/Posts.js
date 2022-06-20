029b668af57dc729575eab541b71ddf73e35efa5
Richard Novoa <56084025+richardNovoa@users.noreply.github.com>
2022-06-20 18:04:15 -0400

    getPosts

---
+++ client/src/components/posts/Posts.js
@@ -0,0 +1,13 @@
+import React, { Fragment, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { getPosts } from '../../actions/post';
+import Spinner from '../layout/Spinner';
+
+const Posts = (props) => {
+	return <div>Posts</div>;
+};
+
+Posts.propTypes = {};
+
+export default Posts;
