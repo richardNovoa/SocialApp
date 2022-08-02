7304df25d97610ac5fda3f0534a907bd6b201f9b
Richard Novoa <56084025+richardNovoa@users.noreply.github.com>
2022-08-02 16:13:11 -0400

    add likes and remove likes

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { GET_POSTS, POST_ERROR } from './types';
+import { GET_POSTS, POST_ERROR, UPDATE_LIKES } from './types';
 import setAuthToken from '../utils/setAuthToken';
 
 //Get Posts
@@ -17,6 +17,48 @@ export const getPosts = () => async (dispatch) => {
 		});
 	} catch (err) {
 		console.log('error');
+		dispatch({
+			type: POST_ERROR,
+			payload: { msg: err.response.statusText, status: err.response.status },
+		});
+	}
+};
+
+//Add like
+
+export const addLike = (id) => async (dispatch) => {
+	if (localStorage.token) {
+		setAuthToken(localStorage.token);
+	}
+	try {
+		const res = await axios.put(`http://localhost:5001/api/posts/like/${id}`);
+
+		dispatch({
+			type: UPDATE_LIKES,
+			payload: { id, likes: res.data },
+		});
+	} catch (err) {
+		dispatch({
+			type: POST_ERROR,
+			payload: { msg: err.response.statusText, status: err.response.status },
+		});
+	}
+};
+
+//Remove like
+
+export const removeLike = (id) => async (dispatch) => {
+	if (localStorage.token) {
+		setAuthToken(localStorage.token);
+	}
+	try {
+		const res = await axios.put(`http://localhost:5001/api/posts/unlike/${id}`);
+
+		dispatch({
+			type: UPDATE_LIKES,
+			payload: { id, likes: res.data },
+		});
+	} catch (err) {
 		dispatch({
 			type: POST_ERROR,
 			payload: { msg: err.response.statusText, status: err.response.status },
