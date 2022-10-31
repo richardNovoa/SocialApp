70bba67662b40eebab00f2575bc09156de18ae21
Richard Novoa <56084025+richardNovoa@users.noreply.github.com>
2022-10-31 13:08:49 -0400

    setting up for deploy

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-const express = require("express");
-const connectDB = require("./config/db");
-const bodyParser = require("body-parser");
-const cors = require("cors");
+const express = require('express');
+const connectDB = require('./config/db');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const path = require('path');
 
 const app = express();
 //cors
@@ -13,13 +14,20 @@ connectDB();
 app.use(express.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => res.send("API Running"));
-
 //define routes
-app.use("/api/auth", require("./routes/api/auth"));
-app.use("/api/posts", require("./routes/api/posts"));
-app.use("/api/profile", require("./routes/api/profile"));
-app.use("/api/users", require("./routes/api/users"));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/users', require('./routes/api/users'));
+
+//Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+	//Set static folder
+	app.use(express.static('client/build'));
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
 
 const PORT = process.env.PORT || 5001;
 
